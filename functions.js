8976d1becaa3e558c3a7e7c10890328791ca617e
AndrewWW2021 <78996605+AndrewWW2021@users.noreply.github.com>
2021-02-14 17:49:04 +0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+function calc() {
+  let first = document.getElementById("first").value;
+  let second = document.getElementById("second").value;
+  first.split(' ');
+  second.split(' ');
+
+  let res = first + second;
+
+
+  alert(first[2]);
+}
